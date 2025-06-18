7c9cb3700c8d7d0d253fdf4995b47a044ba46301
turanak111 <turanak111@gmail.com>
2025-06-19 00:04:07 +0300

    added playermovement script. uses addtorque function to rotate player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;    
+    [SerializeField] float torqueamount;
+    void Start()
+    {
+         rigidbody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2D.AddTorque(torqueamount);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody2D.AddTorque(-torqueamount);
+        }
+    }
+}
